a0e7a15f8886b167d555380256594f2f480cf5ed
Karl-Johan Sjögren <karl-johan.sjogren@xlent.se>
2021-08-10 12:36:42 +0200

    Renamed test file

---
+++ test/Arbetsprov.CSharp.Web.Tests/Services/WeatherServiceTests.cs
@@ -0,0 +1,15 @@
+using Arbetsprov.CSharp.Web.Services;
+using Xunit;
+
+namespace Arbetsprov.CSharp.Web.Tests.Services
+{
+    public class WeatherServiceTests
+    {
+        [Fact]
+        public void CanConstruct()
+        {
+            var service = new WeatherService();
+            Assert.NotNull(service);
+        }
+    }
+}
